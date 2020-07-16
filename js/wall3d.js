7ce12daae46d05175bea388655f63d6c88f66129
maybeb50 <public.maybe.b50@gmail.com>
2020-07-16 14:01:08 +0900

    3D 스크롤 인터렉션 : 스크롤에 따른 상단바 작업

--- js/wall3d.js
+++ js/wall3d.js
@@ -1,19 +1,21 @@
 (function() {
     const houseElem = document.querySelector('.house');
+    const barElem = document.querySelector('.progress__bar');
     let maxScrollValue;
     
     function resizeHandler() {
         maxScrollValue = document.body.offsetHeight - window.innerHeight;
     }
 
-    window.addEventListener('scroll', function() {
+    window.addEventListener('scroll', function () {
         // document.body.offsetHeight : 문서의 전체 높이 
-        // pageYoffset : 현재 스크롤의 높이값
+        // pageYoffset : 현재 스크롤의 위치값
         // window.innerHegiht : 윈도우 화면 높이
-        const zMove = pageYOffset / maxScrollValue * 980 - 490;
-
-        console.log(maxScrollValue);
+        const scrollPer = pageYOffset / maxScrollValue;
+        const zMove = scrollPer * 980 - 490;
         houseElem.style.transform = 'translateZ('+ zMove +'vw )';
+
+        barElem.style.width = scrollPer * 100 +'%';
     });
 
     window.addEventListener('resize', resizeHandler);

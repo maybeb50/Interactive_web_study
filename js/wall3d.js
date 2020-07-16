0c381791e779263e89c83564aa32c8a4b26e83b2
maybeb50 <public.maybe.b50@gmail.com>
2020-07-16 16:07:18 +0900

    mousemove event 추가

--- js/wall3d.js
+++ js/wall3d.js
@@ -1,6 +1,8 @@
 (function() {
     const houseElem = document.querySelector('.house');
     const barElem = document.querySelector('.progress__bar');
+    const stageElem = document.querySelector('.stage');
+    const mousePos = {x: 0, y: 0};
     let maxScrollValue;
     
     function resizeHandler() {
@@ -8,14 +10,21 @@
     }
 
     window.addEventListener('scroll', function () {
-        // document.body.offsetHeight : 문서의 전체 높이 
-        // pageYoffset : 현재 스크롤의 위치값
-        // window.innerHegiht : 윈도우 화면 높이
         const scrollPer = pageYOffset / maxScrollValue;
         const zMove = scrollPer * 980 - 490;
         houseElem.style.transform = 'translateZ('+ zMove +'vw )';
 
+        // progress bar 
         barElem.style.width = scrollPer * 100 +'%';
+    });
+
+    window.addEventListener('mousemove', function (e) {
+        mousePos.x = -1 + (e.clientX / window.innerWidth) * 2; 
+        mousePos.y = 1 - (e.clientY / window.innerHeight) * 2; 
+
+        stageElem.style.transform = 'rotateX('+ (mousePos.y * 8) +'deg) rotateY('+ (mousePos.x * 8) +'deg)';
+
+        console.log(mousePos.x * 5, mousePos.y* 5)
     });
 
     window.addEventListener('resize', resizeHandler);
